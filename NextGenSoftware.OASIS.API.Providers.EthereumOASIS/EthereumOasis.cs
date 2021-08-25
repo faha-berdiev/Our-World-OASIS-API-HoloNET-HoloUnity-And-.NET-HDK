342595b5fe4d30a3c3a584e7305c589f953a5cd7
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-25 17:43:54 +0500

    Added main class

---
+++ NextGenSoftware.OASIS.API.Providers.EthereumOASIS/EthereumOasis.cs
@@ -0,0 +1,10 @@
+﻿using NextGenSoftware.OASIS.API.Core;
+using NextGenSoftware.OASIS.API.Core.Interfaces;
+
+namespace NextGenSoftware.OASIS.API.Providers.EthereumOASIS
+{
+    public class EthereumOasis
+    {
+        
+    }
+}
