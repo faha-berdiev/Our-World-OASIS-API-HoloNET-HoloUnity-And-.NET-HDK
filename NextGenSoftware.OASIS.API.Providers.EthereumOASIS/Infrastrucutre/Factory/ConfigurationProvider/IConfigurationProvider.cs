e450950bddd25aad6a925e5b189e5f7e2348a77b
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-27 03:26:51 +0500

    added configuration provider

---
+++ NextGenSoftware.OASIS.API.Providers.EthereumOASIS/Infrastrucutre/Factory/ConfigurationProvider/IConfigurationProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace NextGenSoftware.OASIS.API.Providers.EthereumOASIS.Infrastrucutre.Factory.ConfigurationProvider
+{
+    public interface IConfigurationProvider
+    {
+        Task SetKey(string key, string value);
+        Task<string> GetKey(string key);
+    }
+}
