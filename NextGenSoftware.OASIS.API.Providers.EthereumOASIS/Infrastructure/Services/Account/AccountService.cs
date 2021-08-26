d0e3c658cb95a0e65a52d21f26e8ff425f342c7a
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-08-27 04:00:52 +0500

    lock and unlock account

--- NextGenSoftware.OASIS.API.Providers.EthereumOASIS/Infrastructure/Services/Account/AccountService.cs
+++ NextGenSoftware.OASIS.API.Providers.EthereumOASIS/Infrastructure/Services/Account/AccountService.cs
@@ -1,4 +1,6 @@
 ﻿using System.Threading.Tasks;
+using Nethereum.Hex.HexTypes;
+using Nethereum.RPC.Eth;
 using Nethereum.Web3;
 
 namespace NextGenSoftware.OASIS.API.Providers.EthereumOASIS.Infrastructure.Services.Account
@@ -15,6 +17,16 @@ namespace NextGenSoftware.OASIS.API.Providers.EthereumOASIS.Infrastructure.Servi
         {
             var accountContent = await _web3.Personal.NewAccount.SendRequestAsync(password);
             return new Nethereum.Web3.Accounts.Account(accountContent);
+        }
+
+        public async Task LockAccount(string password)
+        {
+            await _web3.Personal.LockAccount.SendRequestAsync(password);
+        }
+
+        public async Task UnLockAccount(EthCoinBase coinBase, string password)
+        {
+            await _web3.Personal.UnlockAccount.SendRequestAsync(coinBase, password);
         }
     }
 }
--- NextGenSoftware.OASIS.API.Providers.EthereumOASIS/Infrastructure/Services/Account/IAccountService.cs
+++ NextGenSoftware.OASIS.API.Providers.EthereumOASIS/Infrastructure/Services/Account/IAccountService.cs
@@ -1,9 +1,12 @@
 ﻿using System.Threading.Tasks;
+using Nethereum.RPC.Eth;
 
 namespace NextGenSoftware.OASIS.API.Providers.EthereumOASIS.Infrastructure.Services.Account
 {
     public interface IAccountService
     {
         Task<Nethereum.Web3.Accounts.Account> CreateAccount(string password);
+        Task LockAccount(string password);
+        Task UnLockAccount(EthCoinBase coinBase, string password);
     }
 }
