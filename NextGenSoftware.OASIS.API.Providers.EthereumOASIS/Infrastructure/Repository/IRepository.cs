710e11fa3d6478f25cb3ace5230b8ca968f5ca93
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-09-02 17:09:21 +0500

    IRepository added

---
+++ NextGenSoftware.OASIS.API.Providers.EthereumOASIS/Infrastructure/Repository/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NextGenSoftware.OASIS.API.Providers.EthereumOASIS.Infrastructure.Repository
+{
+    public interface IRepository<T>
+    {
+        Task Create(T entity);
+        Task Update(T entity);
+        
+        Task<T> Get(Guid id);
+        Task<T> Get(string providerKey);
+
+        Task<IEnumerable<T>> GetAll();
+        
+        Task Delete(Guid id);
+        Task Delete(string providerKey);
+    }
+}
