1188d81e51eea37fd462e93afd43d6fef4c384b1
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-09-02 18:09:06 +0500

    CreateAccount new method created

--- NextGenSoftware.OASIS.API.Providers.EthereumOASIS/Infrastructure/Services/Account/AccountService.cs
+++ NextGenSoftware.OASIS.API.Providers.EthereumOASIS/Infrastructure/Services/Account/AccountService.cs
@@ -1,18 +1,36 @@
 ﻿using System.Threading.Tasks;
-using Nethereum.Hex.HexTypes;
 using Nethereum.RPC.Eth;
 using Nethereum.Web3;
+using NextGenSoftware.OASIS.API.Providers.EthereumOASIS.Infrastructure.Factory.ConfigurationProvider;
 
 namespace NextGenSoftware.OASIS.API.Providers.EthereumOASIS.Infrastructure.Services.Account
 {
     public class AccountService : IAccountService
     {
-        private readonly Web3 _web3;
+        private readonly IConfigurationProvider _configurationProvider;
+        private Web3 _web3;
         public AccountService()
         {
+            _configurationProvider = ConfigurationFactory.GetLocalStorageConfigurationProvider();
             _web3 = new Web3();
         }
-        
+
+        public async Task<Nethereum.Web3.Accounts.Account> CreateAccount<T>(T entity)
+        {
+            var address = await _configurationProvider.GetKey("NethereumAddress");
+            var privateKey = await _configurationProvider.GetKey("NethereumPrivateKey");
+            var nethereumHostUri = await _configurationProvider.GetKey("NethereumHostUri");
+            var nethereumAbi = await _configurationProvider.GetKey("NethereumABI");
+            
+            var account = new Nethereum.Web3.Accounts.Account(privateKey);
+            _web3 = new Web3(account, nethereumHostUri);
+            
+            var oasisContract = _web3.Eth.GetContract(nethereumAbi, address);
+            var newAccountFunction = oasisContract.GetFunction("createAccount");
+            var accountContent = await newAccountFunction.CallAsync<string>(entity);
+            return new Nethereum.Web3.Accounts.Account(accountContent);
+        }
+
         public async Task<Nethereum.Web3.Accounts.Account> CreateAccount(string password)
         {
             var accountContent = await _web3.Personal.NewAccount.SendRequestAsync(password);
--- NextGenSoftware.OASIS.API.Providers.EthereumOASIS/Infrastructure/Services/Account/IAccountService.cs
+++ NextGenSoftware.OASIS.API.Providers.EthereumOASIS/Infrastructure/Services/Account/IAccountService.cs
@@ -5,6 +5,7 @@ namespace NextGenSoftware.OASIS.API.Providers.EthereumOASIS.Infrastructure.Servi
 {
     public interface IAccountService
     {
+        Task<Nethereum.Web3.Accounts.Account> CreateAccount<T>(T entity);
         Task<Nethereum.Web3.Accounts.Account> CreateAccount(string password);
         Task LockAccount(string password);
         Task UnLockAccount(EthCoinBase coinBase, string password);
