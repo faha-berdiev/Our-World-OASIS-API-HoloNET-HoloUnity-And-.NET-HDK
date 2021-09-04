2e25dcd80a4d25851eb5224590fd3161a0c5338d
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-09-04 15:50:38 +0500

    added entitmanager

---
+++ NextGenSoftware.OASIS.API.Providers.EthereumOASIS/Infrastructure/Services/EntityManager/EntityManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using NextGenSoftware.OASIS.API.Providers.EthereumOASIS.Enums;
+using NextGenSoftware.OASIS.API.Providers.EthereumOASIS.Infrastructure.Services.EntityMigrator;
+using NextGenSoftware.OASIS.API.Providers.EthereumOASIS.Infrastructure.Services.EntityStreamer;
+using NextGenSoftware.OASIS.API.Providers.EthereumOASIS.Models.Entity;
+
+namespace NextGenSoftware.OASIS.API.Providers.EthereumOASIS.Infrastructure.Services.EntityManager
+{
+    public class EntityManager<T> : IEntityManager<T> where T : new()
+    {
+        private readonly IEntityStreamer _entityStreamer;
+        private readonly IEntityMigrator<T> _entityMigrator;
+
+        public EntityManager()
+        {
+            _entityStreamer = new EntityStreamer.EntityStreamer();
+            _entityMigrator = new EntityMigrator<T>();
+        }
+        
+        public async Task<T> Get(EntityReference reference)
+        {
+            if (reference == null)
+                throw new ArgumentNullException();
+
+            var response = await _entityStreamer.Download(reference);
+            if (response.Status == ResponseStatus.Failed)
+                return new T();
+            var entityResponse = await _entityMigrator.GetEntity(response.Payload.Content);
+            return entityResponse.Status == ResponseStatus.Failed ? new T() : entityResponse.Payload;
+        }
+
+        public async Task<EntityReference> Upload(T entity)
+        {
+            if(entity == null)
+                throw new ArgumentNullException();
+
+            var requestContent = await _entityMigrator.GetEntityContent(entity);
+            if (requestContent.Status == ResponseStatus.Failed)
+                return null;
+            var uploadResponse = await _entityStreamer.Upload(requestContent.Payload);
+            return uploadResponse.Status == ResponseStatus.Failed ? null : uploadResponse.Payload;
+        }
+    }
+}
---
+++ NextGenSoftware.OASIS.API.Providers.EthereumOASIS/Infrastructure/Services/EntityManager/IEntityManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using NextGenSoftware.OASIS.API.Providers.EthereumOASIS.Models.Entity;
+
+namespace NextGenSoftware.OASIS.API.Providers.EthereumOASIS.Infrastructure.Services.EntityManager
+{
+    public interface IEntityManager<T> where T : new ()
+    {
+        Task<T> Get(EntityReference reference);
+        Task<EntityReference> Upload(T entity);
+    }
+}
