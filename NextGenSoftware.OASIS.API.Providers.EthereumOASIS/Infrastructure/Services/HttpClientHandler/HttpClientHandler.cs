937fc82692332479cdec124e791b91574afe0c35
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-09-06 16:06:30 +0500

    added client handler

---
+++ NextGenSoftware.OASIS.API.Providers.EthereumOASIS/Infrastructure/Services/HttpClientHandler/HttpClientHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NextGenSoftware.OASIS.API.Providers.EthereumOASIS.Infrastructure.Services.HttpClientHandler
+{
+    public class HttpClientHandler : IHttpClientHandler
+    {
+        private readonly HttpClient _httpClient;
+
+        public HttpClientHandler()
+        {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<HttpResponseMessage> SendAsync(HttpRequestMessage requestMessage)
+        {
+            return await _httpClient.SendAsync(requestMessage);
+        }
+    }
+}
---
+++ NextGenSoftware.OASIS.API.Providers.EthereumOASIS/Infrastructure/Services/HttpClientHandler/IHttpClientHandler.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NextGenSoftware.OASIS.API.Providers.EthereumOASIS.Infrastructure.Services.HttpClientHandler
+{
+    public interface IHttpClientHandler
+    {
+        Task<HttpResponseMessage> SendAsync(HttpRequestMessage requestMessage);
+    }
+}
