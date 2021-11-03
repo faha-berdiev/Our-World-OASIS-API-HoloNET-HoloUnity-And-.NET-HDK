a9f31e028dd89aa1221ac8d05b4934d98f3da700
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2021-11-03 23:03:48 +0500

    created test harness for cargo project

---
+++ NextGenSoftware.OASIS.API.Providers.CargoOASIS.TestHarness/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NextGenSoftware.OASIS.API.Providers.CargoOASIS.TestHarness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
